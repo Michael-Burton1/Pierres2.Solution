8c1a39b5be0f2e27eb4082d3eca6979d3540d0cc
Michael Burton <micbur1@gmail.com>
2021-06-06 17:08:26 -0700

    fixed using statements for ApplicationUser model

--- Bakery/Models/ApplicationUser.cs
+++ Bakery/Models/ApplicationUser.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using RecipeBox.Models;
+using Bakery.Models;
 using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
